b7fb0fb8963d996f9e26ef34e1fb94b1f6550ea9
vinicius menezes <viniciusm_azevedo@hotmail.com>
2019-05-15 18:59:35 -0300

    Treinamento Xamarim Forms

---
+++ App_ControleXF/App_ControleXF/Model/ProgressBarPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_ControleXF.Model
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProgressBarPage : ContentPage
+    {
+        public ProgressBarPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Modificar (object sender, EventArgs args)
+        {
+            Bar1.Progress = 1;
+            Bar2.ProgressTo(1, 5000, Easing.Linear);
+            Bar3.ProgresTo(1, 5000, Easing.SpringIn);
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ App_ControleXF/App_ControleXF/Views/Master.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App_ControleXF.Model;
+
+namespace App_ControleXF.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+        private void GoActivityIndicatorPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.ActivityIndicatorPage());
+            IsPresented = false;
+        }
+        private void GoProgressBarPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.ProgressBarPage());
+            IsPresented = false;
+        }
+        private void GoBoxViewPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.BoxViewPage());
+            IsPresented = false;
+        }
+        private void GoLabelPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.LabelPage());
+            IsPresented = false;
+        }
+        private void GoButtonPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.ButtonPage());
+            IsPresented = false;
+        }
+        private void GoEntryEditorPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.EntryEditorPage());
+            IsPresented = false;
+        }
+        private void GoDatePickerPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.DatePickerPage());
+            IsPresented = false;
+        }
+        private void GoTimePickerPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.TimePickerPage());
+            IsPresented = false;
+        }
+        private void GoPickerPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.PickerPage());
+            IsPresented = false;
+        }
+        private void GoSearchBarPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.SearchBarPage());
+            IsPresented = false;
+        }
+        private void GoSliderStepperBarPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.SliderStepperPage());
+            IsPresented = false;
+        }
+        private void GoSwitchPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.SwitchPage());
+            IsPresented = false;
+        }
+        private void GoImagePage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.ImagePage());
+            IsPresented = false;
+        }
+        private void GoListViewPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.ListViewPage());
+            IsPresented = false;
+        }
+        private void GoTableViewPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.TableViewPage());
+            IsPresented = false;
+        }
+        private void GoWebViewPage(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Model.WebViewPage());
+            IsPresented = false;
+        }
+    }
+}
